1ac36b20823dbd4f5550451d88a6cd939d2f5ee3
Joaquin Bejar <jb@taunais.com>
2024-11-21 18:07:34 +0100

    Refactor websocket example with detailed main function
    Changed the package to main and added a comprehensive main function. The new main function includes functionalities for fetching configurations, creating a client, and executing multiple operations like fetching summaries and subscribing to different notifications.

--- cmd/examples/websocket_example/main.go
+++ cmd/examples/websocket_example/main.go
@@ -1,1 +1,186 @@
-package websocket_example
+package main
+
+import (
+	"deribit-api/internal/websocket"
+	"deribit-api/pkg/deribit"
+	"deribit-api/pkg/models"
+	"log"
+	"os"
+	"strconv"
+)
+
+func getEnvWithDefault(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
+
+func getConfig() *deribit.Configuration {
+	autoReconnect, _ := strconv.ParseBool(getEnvWithDefault("DERIBIT_AUTO_RECONNECT", "true"))
+	debugMode, _ := strconv.ParseBool(getEnvWithDefault("DERIBIT_DEBUG_MODE", "true"))
+
+	return &deribit.Configuration{
+		Addr:          deribit.TestBaseURL,
+		ApiKey:        getEnvWithDefault("DERIBIT_API_KEY", ""),
+		SecretKey:     getEnvWithDefault("DERIBIT_API_SECRET", ""),
+		AutoReconnect: autoReconnect,
+		DebugMode:     debugMode,
+	}
+}
+
+func main() {
+	cfg := getConfig()
+	println(cfg.ApiKey)
+	println(cfg.SecretKey)
+	client := websocket.New(cfg)
+
+	_, g_err := client.GetTime()
+	if g_err != nil {
+		return
+	}
+	_, t_err := client.Test()
+	if t_err != nil {
+		return
+	}
+
+	var err error
+
+	// GetBookSummaryByCurrency
+	getBookSummaryByCurrencyParams := &models.GetBookSummaryByCurrencyParams{
+		Currency: "BTC",
+		Kind:     "future",
+	}
+	var getBookSummaryByCurrencyResult []models.BookSummary
+	getBookSummaryByCurrencyResult, err = client.GetBookSummaryByCurrency(getBookSummaryByCurrencyParams)
+	if err != nil {
+		log.Printf("%v", err)
+		return
+	}
+	log.Printf("%v", getBookSummaryByCurrencyResult)
+
+	// GetOrderBook
+	getOrderBookParams := &models.GetOrderBookParams{
+		InstrumentName: "BTC-PERPETUAL",
+		Depth:          5,
+	}
+	var getOrderBookResult models.GetOrderBookResponse
+	getOrderBookResult, err = client.GetOrderBook(getOrderBookParams)
+	if err != nil {
+		log.Printf("%v", err)
+		return
+	}
+	log.Printf("%v", getOrderBookResult)
+
+	// GetPosition
+	getPositionParams := &models.GetPositionParams{
+		InstrumentName: "BTC-PERPETUAL",
+	}
+	var getPositionResult models.Position
+	getPositionResult, err = client.GetPosition(getPositionParams)
+	if err != nil {
+		log.Printf("%v", err)
+		return
+	}
+	log.Printf("%v", getPositionResult)
+
+	// Buy
+	guyParams := &models.BuyParams{
+		InstrumentName: "BTC-PERPETUAL",
+		Amount:         40,
+		Price:          6000.0,
+		Type:           "limit",
+	}
+	var buyResult models.BuyResponse
+	buyResult, err = client.Buy(guyParams)
+	if err != nil {
+		log.Printf("%v", err)
+		return
+	}
+	log.Printf("%v", buyResult)
+
+	// Subscribe
+	client.On("announcements", func(e *models.AnnouncementsNotification) {
+
+	})
+	client.On("book.ETH-PERPETUAL.100.1.100ms", func(e *models.OrderBookGroupNotification) {
+
+	})
+	client.On("book.BTC-PERPETUAL.100ms", func(e *models.OrderBookNotification) {
+
+	})
+	client.On("book.BTC-PERPETUAL.raw", func(e *models.OrderBookRawNotification) {
+
+	})
+	client.On("deribit_price_index.btc_usd", func(e *models.DeribitPriceIndexNotification) {
+
+	})
+	client.On("deribit_price_ranking.btc_usd", func(e *models.DeribitPriceRankingNotification) {
+
+	})
+	client.On("estimated_expiration_price.btc_usd", func(e *models.EstimatedExpirationPriceNotification) {
+
+	})
+	client.On("markprice.options.btc_usd", func(e *models.MarkpriceOptionsNotification) {
+
+	})
+	client.On("perpetual.BTC-PERPETUAL.raw", func(e *models.PerpetualNotification) {
+
+	})
+	client.On("quote.BTC-PERPETUAL", func(e *models.QuoteNotification) {
+
+	})
+	client.On("ticker.BTC-PERPETUAL.raw", func(e *models.TickerNotification) {
+
+	})
+	client.On("trades.BTC-PERPETUAL.raw", func(e *models.TradesNotification) {
+
+	})
+
+	client.On("user.changes.BTC-PERPETUAL.raw", func(e *models.UserChangesNotification) {
+
+	})
+	client.On("user.changes.future.BTC.raw", func(e *models.UserChangesNotification) {
+
+	})
+	client.On("user.orders.BTC-PERPETUAL.raw", func(e *models.UserOrderNotification) {
+
+	})
+	client.On("user.orders.future.BTC.100ms", func(e *models.UserOrderNotification) {
+
+	})
+	client.On("user.portfolio.btc", func(e *models.PortfolioNotification) {
+
+	})
+	client.On("user.trades.BTC-PERPETUAL.raw", func(e *models.UserTradesNotification) {
+
+	})
+	client.On("user.trades.future.BTC.100ms", func(e *models.UserTradesNotification) {
+
+	})
+
+	client.Subscribe([]string{
+		//"announcements",
+		//"book.BTC-PERPETUAL.none.10.100ms",	// none/1,2,5,10,25,100,250
+		//"book.BTC-PERPETUAL.100ms",	// type: snapshot/change
+		"book.BTC-PERPETUAL.raw",
+		//"deribit_price_index.btc_usd",
+		//"deribit_price_ranking.btc_usd",
+		//"estimated_expiration_price.btc_usd",
+		//"markprice.options.btc_usd",
+		//"perpetual.BTC-PERPETUAL.raw",
+		//"quote.BTC-PERPETUAL",
+		//"ticker.BTC-PERPETUAL.raw",
+		//"trades.BTC-PERPETUAL.raw",
+		//"user.changes.BTC-PERPETUAL.raw",
+		//"user.changes.future.BTC.raw",
+		//"user.orders.BTC-PERPETUAL.raw",
+		//"user.orders.future.BTC.100ms",
+		//"user.portfolio.btc",
+		//"user.trades.BTC-PERPETUAL.raw",
+		//"user.trades.future.BTC.100ms",
+	})
+
+	forever := make(chan bool)
+	<-forever
+}
